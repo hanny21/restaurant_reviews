1d09454059153f9bc0a37ad79835844f354fb1eb
Hana Klingová <hana.klingova@gmail.com>
2018-06-25 00:06:58 +0200

    change sw

--- js/sw.js
+++ js/sw.js
@@ -1,52 +1,58 @@
 /* eslint-disable */
-var staticCacheName = 'restaurant-reviews-v1';
+var staticCacheName = 'restaurant-cache-v1';
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(staticCacheName).then(function(cache) {
-      return cache.addAll([
-        '/',
-        './js/main.js',
-        './js/dbhelper.js',
-        './js/restaurant_info.js',
-        './data/restaurants.json',
-        './css/styles.css',
-        './img/1.png',
-        './img/2.png',
-        './img/3.png',
-        './img/4.png',
-        './img/5.png',
-        './img/6.png',
-        './img/7.png',
-        './img/8.png',
-        './img/9.png',
-        './img/10.png',
-        'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css'
-      ]);
-    })
-  );
-});
+let urlToCache = [
+    '/',
+    './restaurant.html',
+    './css/styles.css',
+    './data/restaurants.json',
+    './img/1.jpg',
+    './img/2.jpg',
+    './img/3.jpg',
+    './img/4.jpg',
+    './img/5.jpg',
+    './img/6.jpg',
+    './img/7.jpg',
+    './img/8.jpg',
+    './img/9.jpg',
+    './img/10.jpg',
+    './js/main.js',
+    './js/restaurant_info.js',
+    './js/dbhelper.js',
+    'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css'
+];
+self.addEventListener('install', function (event) {
 
+    event.waitUntil(
+        caches.open(staticCacheName).then(function (cache) {
+            console.log(cache);
+            return cache.addAll(urlToCache);
 
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          return cacheName.startsWith('restaurant-reviews-') &&
-                 cacheName != staticCacheName;
-        }).map(function(cacheName) {
-          return caches.delete(cacheName);
+        }).catch(erroe => {
+            console.log(erroe);
         })
-      );
-    })
-  );
+    );
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function (cacheName) {
+                    return cacheName.startsWith('restaurant-cache') &&
+                        cacheName != staticCacheName;
+                }).map(function (cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
 });
