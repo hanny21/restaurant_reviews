ffb671922aa3bf600c363c3282b933f451138995
Hana Klingová <hana.klingova@gmail.com>
2018-06-24 23:19:30 +0200

    make changes suggested by accessibility autdit in chrome devtools

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -89,6 +89,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
 
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
+  image.setAttribute('alt', 'image of ' + restaurant.name + ' restaurant');
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
 
   const cuisine = document.getElementById('restaurant-cuisine');
