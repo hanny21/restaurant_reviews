4e2410ce1f3dacbbc98b38e6367bcf51de6a227e
Hana Klingová <hana.klingova@gmail.com>
2018-06-25 23:04:13 +0200

    adjust cacheFiles array

--- sw.js
+++ sw.js
@@ -1,24 +1,23 @@
 var cacheName = 'restaurant-reviews-v1';
 
 let cacheFiles = [
-  '/',
   './index.html',
   './restaurant.html',
-  './styles.css',
+  './css/styles.css',
   './data/restaurants.json',
   './js/dbhelper.js',
   './js/main.js',
   './js/restaurant_info.js',
-  './img/1.png',
-  './img/2.png',
-  './img/3.png',
-  './img/4.png',
-  './img/5.png',
-  './img/6.png',
-  './img/7.png',
-  './img/8.png',
-  './img/9.png',
-  './img/10.png',
+  './img/1.jpg',
+  './img/2.jpg',
+  './img/3.jpg',
+  './img/4.jpg',
+  './img/5.jpg',
+  './img/6.jpg',
+  './img/7.jpg',
+  './img/8.jpg',
+  './img/9.jpg',
+  './img/10.jpg',
   'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css'
 ];
 
