c91f22e16370770c167f6e376a85419681b3d6d4
Hana Klingová <hana.klingova@gmail.com>
2018-06-25 21:43:50 +0200

    add SW install event

--- js/sw.js
+++ js/sw.js
@@ -1,38 +1,37 @@
-/* eslint-disable */
-let cacheName = 'restaurant-reviews-v1';
+let cacheName = "restaurant-reviews-v1";
 
 var cacheFiles = [
-  './',
-  './index.html',
-  './restaurant.html',
-  './styles.css',
-  './data/restaurants.json',
-  './js/dbhelper.js',
-  './js/main.js',
-  './js/restaurant_info.js',
-  './img/1.png',
-  './img/2.png',
-  './img/3.png',
-  './img/4.png',
-  './img/5.png',
-  './img/6.png',
-  './img/7.png',
-  './img/8.png',
-  './img/9.png',
-  './img/10.png',
-  'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css'
-]
+  "./",
+  "./index.html",
+  "./restaurant.html",
+  "./styles.css",
+  "./data/restaurants.json",
+  "./js/dbhelper.js",
+  "./js/main.js",
+  "./js/restaurant_info.js",
+  "./img/1.png",
+  "./img/2.png",
+  "./img/3.png",
+  "./img/4.png",
+  "./img/5.png",
+  "./img/6.png",
+  "./img/7.png",
+  "./img/8.png",
+  "./img/9.png",
+  "./img/10.png",
+  "https://unpkg.com/leaflet@1.3.1/dist/leaflet.css"
+];
 
-self.addEventListener('install', (e) => {
-  console.log('***SW installed')
+self.addEventListener("install", (e) => {
+  console.log("***SW installed")
   e.waitUntil(
     caches.open(cacheName).then((cache) => {
-      console.log('SW is caching files');
+      console.log("SW is caching files");
       return cache.addAll(cacheFiles);
     })
-  )
-})
+  );
+});
 
-self.addEventListener('activate', (e) => console.log('***SW activated'))
+self.addEventListener("activate", (e) => console.log("***SW activated"))
 
-self.addEventListener('fetch', (e) => console.log('***SW fetching', e.request.url))
+self.addEventListener("fetch", (e) => console.log("***SW fetching", e.request.url))
