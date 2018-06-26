822a94ea71da596e39519f70e3d8d9ac1ce0fae2
Hana Klingová <hana.klingova@gmail.com>
2018-06-26 09:43:01 +0200

    change cachefiles

--- sw.js
+++ sw.js
@@ -1,24 +1,24 @@
 const cacheName = 'restaurant-reviews-v1';
 
 const cacheFiles = [
-  '/',
-  '/index.html',
-  '/restaurant.html',
-  '/css/styles.css',
-  '/data/restaurants.json',
-  '/js/dbhelper.js',
-  '/js/main.js',
-  '/js/restaurant_info.js',
-  '/img/1.jpg',
-  '/img/2.jpg',
-  '/img/3.jpg',
-  '/img/4.jpg',
-  '/img/5.jpg',
-  '/img/6.jpg',
-  '/img/7.jpg',
-  '/img/8.jpg',
-  '/img/9.jpg',
-  '/img/10.jpg',
+  './',
+  './index.html',
+  './restaurant.html',
+  './css/styles.css',
+  './data/restaurants.json',
+  './js/dbhelper.js',
+  './js/main.js',
+  './js/restaurant_info.js',
+  './img/1.jpg',
+  './img/2.jpg',
+  './img/3.jpg',
+  './img/4.jpg',
+  './img/5.jpg',
+  './img/6.jpg',
+  './img/7.jpg',
+  './img/8.jpg',
+  './img/9.jpg',
+  './img/10.jpg',
   'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css'
 ];
 
@@ -48,8 +48,7 @@ self.addEventListener('activate', (e) =>{
 self.addEventListener('fetch', (e) => {
   e.respondWith(e.request)
     .then((response) => {
-      if(response) {
-        return response;
+      if(response) {return response;
       } return fetch(e.request);
     });
 });
